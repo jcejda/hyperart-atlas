d8861ff23de754f88205e5fbd2c6a6fbe2753c1d
Jackson Cejda <cejdajackson@gmail.com>
2025-12-30 09:53:42 -0600

    Populate more details in map UI card element

---
+++ frontend/src/components/HyperartPopupCard.jsx
@@ -0,0 +1,68 @@
+function HyperartPopupCard({ hyperart }) {
+  const {
+    id,
+    title,
+    latitude,
+    longitude,
+    thumbnailUrl,
+    uploadedBy,
+  } = hyperart;
+
+  return (
+    <div style={{ width: "200px", fontFamily: "sans-serif" }}>
+      {/* Thumbnail */}
+      {thumbnailUrl && (
+        <img
+          src={thumbnailUrl}
+          alt={title || "Hyperart Thomasson"}
+          style={{
+            width: "100%",
+            height: "120px",
+            objectFit: "cover",
+            borderRadius: "4px",
+            marginBottom: "6px",
+          }}
+        />
+      )}
+
+      {/* Title */}
+      {title && (
+        <div style={{ fontWeight: "bold", marginBottom: "2px" }}>
+          {title}
+        </div>
+      )}
+
+      {/* Uploaded by */}
+      {uploadedBy && (
+        <div
+          style={{
+            fontSize: "12px",
+            color: "#777",
+            marginBottom: "4px",
+          }}
+        >
+          Uploaded by {uploadedBy}
+        </div>
+      )}
+
+      {/* Coordinates */}
+      <div style={{ fontSize: "12px", color: "#555", marginBottom: "6px" }}>
+        {latitude.toFixed(4)}, {longitude.toFixed(4)}
+      </div>
+
+      {/* Link */}
+      <a
+        href={`/hyperart/${id}`}
+        style={{
+          fontSize: "12px",
+          color: "#1d4ed8",
+          textDecoration: "none",
+        }}
+      >
+        View full entry →
+      </a>
+    </div>
+  );
+}
+
+export default HyperartPopupCard;
--- frontend/src/components/WorldMap.jsx
+++ frontend/src/components/WorldMap.jsx
@@ -1,4 +1,15 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import HyperartPopupCard from "./HyperartPopupCard";
+
+const exampleHyperart = {
+  id: 1,
+  title: "Unused Staircase",
+  latitude: 35.6895,
+  longitude: 139.6917,
+  thumbnailUrl:
+    "https://upload.wikimedia.org/wikipedia/commons/8/8c/%E3%83%88%E3%83%9E%E3%82%BD%E3%83%B3%E9%9A%8E%E6%AE%B5_%285104257679%29.jpg",
+  uploadedBy: "agenpei"
+};
 
 function WorldMap() {
   return (
@@ -12,11 +23,9 @@ function WorldMap() {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
-      <Marker position={[35.6895, 139.6917]}>
+      <Marker position={[exampleHyperart.latitude, exampleHyperart.longitude]}>
         <Popup>
-          <strong>Tokyo</strong>
-          <br />
-          Example Hyperart Thomasson
+          <HyperartPopupCard hyperart={exampleHyperart} />
         </Popup>
       </Marker>
     </MapContainer>
@@ -24,4 +33,3 @@ function WorldMap() {
 }
 
 export default WorldMap;
-
