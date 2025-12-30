78cfe8b87e199167c0fd54fe997aace13d17c651
Jackson Cejda <cejdajackson@gmail.com>
2025-12-30 10:18:45 -0600

    Add logic to render multiple pins

--- frontend/src/components/WorldMap.jsx
+++ frontend/src/components/WorldMap.jsx
@@ -1,15 +1,35 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import HyperartPopupCard from "./HyperartPopupCard";
 
-const exampleHyperart = {
-  id: 1,
-  title: "Unused Staircase",
-  latitude: 35.6895,
-  longitude: 139.6917,
-  thumbnailUrl:
-    "https://upload.wikimedia.org/wikipedia/commons/8/8c/%E3%83%88%E3%83%9E%E3%82%BD%E3%83%B3%E9%9A%8E%E6%AE%B5_%285104257679%29.jpg",
-  uploadedBy: "agenpei"
-};
+const hyperartEntries = [
+  {
+    id: 1,
+    title: "Genesis of Hyperart",
+    latitude: 35.6895,
+    longitude: 139.6917,
+    thumbnailUrl:
+      "https://static.wixstatic.com/media/3864b1_2272d7f629504323bad460219d3efd8a~mv2.png/v1/fill/w_756,h_551,al_c,q_90,enc_avif,quality_auto/3864b1_2272d7f629504323bad460219d3efd8a~mv2.png",
+    uploadedBy: "agenpei",
+  },
+  {
+    id: 2,
+    title: "Blocked Doorway",
+    latitude: 51.5074,
+    longitude: -0.1278,
+    thumbnailUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/8/8c/%E3%83%88%E3%83%9E%E3%82%BD%E3%83%B3%E9%9A%8E%E6%AE%B5_%285104257679%29.jpg",
+    uploadedBy: "londonwalker",
+  },
+  {
+    id: 3,
+    title: null, // intentionally unnamed
+    latitude: 40.7128,
+    longitude: -74.006,
+    thumbnailUrl: null,
+    uploadedBy: "anonymous",
+  },
+];
+
 
 function WorldMap() {
   return (
@@ -23,11 +43,16 @@ function WorldMap() {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
-      <Marker position={[exampleHyperart.latitude, exampleHyperart.longitude]}>
-        <Popup>
-          <HyperartPopupCard hyperart={exampleHyperart} />
-        </Popup>
-      </Marker>
+      {hyperartEntries.map((entry) => (
+        <Marker
+          key={entry.id}
+          position={[entry.latitude, entry.longitude]}
+        >
+          <Popup>
+            <HyperartPopupCard hyperart={entry} />
+          </Popup>
+        </Marker>
+      ))}
     </MapContainer>
   );
 }
